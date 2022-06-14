b251fda8f76485526be661133093b2db01355ec3
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-06-14 13:55:53 +0100

    fix heuristic (now calling empty cells field instead of the function

--- takuzu.py
+++ takuzu.py
@@ -271,7 +271,7 @@ class Takuzu(Problem):
             return 0
 
         result = 0
-        for x,y in board.empty_cells():
+        for x,y in board.empty:
             row_count, col_count = board.get_row_count(x), board.get_col_count(y)
             result += line_heuristic(row_count) + line_heuristic(col_count)
         return result / (2*node.state.board.size)
@@ -312,7 +312,7 @@ class Takuzu(Problem):
 if __name__ == "__main__":
     board = Board.parse_instance_from_stdin()
     takuzu = Takuzu(board)
-    goal = depth_first_tree_search(takuzu)
+    goal = greedy_search(takuzu)
     #print("---")
     if goal:
         print(goal.state.board)
